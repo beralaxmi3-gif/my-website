ccf06df12dfce7f32226f40acb058be370744564
beralaxmi3-gif <beralaxmi3@gmail.com>
2026-08-20 15:05:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log("Happy Birthday Website Loaded!");
+window.onload = function() {
+    alert("Happy Birthday!");
+    alert("Happy Birthday to a wonderful friend! May your day be as bright and lovely as your smile, and may this new year bring you endless happiness and beautiful moments. Cheers to celebrating you today!");
+};
